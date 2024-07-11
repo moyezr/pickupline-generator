cfc1a46afde19ab9191fbc339268093d2af86667
Moyez Rabbani <moyezrabbani@gmail.com>
2024-07-12 00:42:41 +0530

    fix redirect url

--- src/actions/login.ts
+++ src/actions/login.ts
@@ -11,7 +11,7 @@ export async function login() {
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider: "google",
     options: {
-      redirectTo: `${process.env.SITE_URL}/auth/callback`,
+      redirectTo: `${process.env.NEXT_PUBLIC_SITE_URL}/auth/callback`,
     },
   })
 
